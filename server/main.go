6f200ecab8ce91570cd8f18e36c8da103b99c94e
JamesCostello-dev <jcotab@gmail.com>
2022-04-30 13:51:21 -0700

    add ignore

--- server/main.go
+++ server/main.go
@@ -3,10 +3,7 @@ package main
 import "github.com/gofiber/fiber/v2"
 
 func main() {
-    app := fiber.New()
-
-    app.Static("/", "../client")
-
-    app.Listen(":3000")
-
+	app := fiber.New()
+	app.Static("/", "../client")
+	app.Listen(":3000")
 }
